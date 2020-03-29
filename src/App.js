928a0a3233f3c0c04bedaac7db2a83081375a588
unknown <576062243@qq.com>
2020-03-29 22:47:03 +0800

    state传值

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Day1 from './components/Day1'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +19,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>父组件</h1>
+      <Day1 />
     </div>
   );
 }
