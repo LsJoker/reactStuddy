8b49be534e2c64645536207749a5892d28e00272
unknown <576062243@qq.com>
2020-03-30 22:04:14 +0800

    day1Study‘

--- src/components/Day1.js
+++ src/components/Day1.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 // import logo from './logo.svg';
 import '../assets/day1.css';
+import logo from '../../src/logo.svg'
 
 /**
  * 1.新建一个组件Day1
@@ -24,13 +25,50 @@ class Day1 extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			msg:'子组件'
+			msg:'子组件',
+			colorClass:'red',
+			arr1:['1','2','3']
 		}
+		this.getData3 = this.getData3.bind(this)
+	}
+	getData1 () {
+		this.setState({
+			msg:'111'
+		})
+	}
+	getData2 = (str)=>{
+		this.setState({
+			msg:str
+		})
+	}
+	getData3(){
+		this.setState({
+			msg:'333'
+		})
 	}
 	render(){
+		let arr1Doc = this.state.arr1.map(function(val,key) {
+			return <h2 key={key}>{val}</h2>
+		})
 	  return (
+		
+		// let listResult=this.state.list.map(function(value,key){
+
+		// 	return <li key={key}>{value}</li>
+		// })
 		<div>
-			<h1>{this.state.msg}</h1>
+			<h2 onClick={this.getData1.bind(this)}>{this.state.msg}</h2>
+			<h2 onClick={this.getData2.bind(this,'123')} className={this.state.colorClass}>{this.state.msg}</h2>
+			<label htmlFor='userName'>用户名</label><input id="userName" />
+			<h2 onClick={this.getData3} style={{'color':this.state.colorClass}}>{this.state.msg}</h2>
+			<img src={logo} style={{'width':'100px','height':'100px'}}/>
+			<img src={require('../../src/logo.svg')} style={{'width':'100px','height':'100px'}}/>
+			{arr1Doc}
+			{
+				this.state.arr1.map(function(val,key) {
+					return <h2 key={key}>{val}</h2>
+				})
+			}
 		</div>
 	  );
 	}
